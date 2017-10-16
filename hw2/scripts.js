c6e51b8d49d82f4434fcce12952120fbae155f78
Ilay <sakhno.iv@gmail.com>
2017-10-17 01:03:48 +0300

    Time filter was implemented

--- hw2/scripts.js
+++ hw2/scripts.js
@@ -1,6 +1,7 @@
-var dataset_Countries2012;
+let dataset_Countries2012;
+let dataset_CountriesByYears;
 
-var sortingOrder = { // true for asc
+let sortingOrder = { // true for asc
     name: true,      // false for desc
     continent: true,
     gdp: true,
@@ -9,7 +10,7 @@ var sortingOrder = { // true for asc
     year: true
 };
 
-var cbContinentsState = {
+let cbContinentsState = {
     Americas: false,
     Africa: false,
     Asia: false,
@@ -17,153 +18,47 @@ var cbContinentsState = {
     Oceania: false
 };
 
-var gdpParseFormatter = (str) =>
-    parseFloat(str.replace('.','').replace('T','1e12').replace('G', '1e9').replace('P', '1e15'));
-var populationParseFormatter = (str) => parseInt(str.replace(/,/g,''));
-var sorter = (a, b, order) => order ? d3.ascending(a, b) : d3.descending(a, b);
+let desireColumns = ['name', 'continent', 'gdp', 'life_expectancy', 'population', 'year'];
 
-var changeCursorState = (header) => header ?
+let gdpParseFormatter = (str) =>
+    parseFloat(str.replace('.', '').replace('P', '1e15').replace('T', '1e12').replace('G', '1e9').replace('M', '1e6'));
+let populationParseFormatter = (str) => parseInt(str.replace(/,/g, ''));
+let sorter = (a, b, order) => order ? d3.ascending(a, b) : d3.descending(a, b);
+
+let changeCursorState = (header) => header ?
     'url(images/down-arrow_2.png), auto' : 'url(images/up-arrow_2.png), auto';
 
-let desireColumns;
+let dataset;
 
-function drawTable() {
-    /*let */desireColumns = getNeededColumnKeys(Object.keys(dataset_Countries2012[0]));
+let tableCleaner = (tableBody) => tableBody.selectAll('tr').remove();
 
-    dataset_Countries2012.forEach(o => formatStringRepresent(o));
+let tableDrawer = () => d3.select('body').append('table').attr('class', 'tableClass');
+let tableHeadDrawer = (table) =>  table.append('thead').attr('class', 'thead');
+let tableBodyDrawer = (table) => table.append('tbody');
 
-    let table = d3.select('body').append('table').attr('class', 'tableClass'),
-        thead = table.append('thead').attr('class', 'thead'),
-        tbody = table.append('tbody');
+let tableCaptionDrawer = (table) => table.append('caption')
+    .html("World Countries Ranking")
+    .attr('class', 'caption');
 
-    table.append('caption')
-        .html("World Countries Ranking")
-        .attr('class', 'caption');
-
+function addTableHeaders(thead, tbody) {
     thead.append('tr').selectAll('th')
         .data(desireColumns)
         .enter()
         .append('th')
         .attr('class', 'tableHeaders')
         .text(d => d)
-        .on('click', function(header, i) { tbody
+        .on('click', function(header) { tbody
             .selectAll('tr')
             .sort((a, b) => sorting(a, b, header)),
-                sortingOrder[header] = !sortingOrder[header],
-                colorizeTableInZebraStyle(tbody),
-                d3.select(this)
-                    .style('cursor', () => changeCursorState(sortingOrder[header]))
+            sortingOrder[header] = !sortingOrder[header],
+            colorizeTableInZebraStyle(tbody),
+            d3.select(this)
+                .style('cursor', () => changeCursorState(sortingOrder[header]))
         })
-        .on('mouseover', function (header, i) {
+        .on('mouseover', function (header) {
             d3.select(this)
                 .style('cursor', () => changeCursorState(sortingOrder[header]))
         });
-
-    let rows = tbody.selectAll('tr.row')
-        .data(dataset_Countries2012)
-        .enter()
-        .append('tr')
-        .each(function (d, i) {
-            d3.select(this)
-                .attr('class', (i % 2 === 0) ? 'even' : 'odd')
-        })
-        .attr('class', 'row');
-
-    // drawTableRows(d3.select("table").select('tbody'), desireColumns, dataset_Countries2012);
-
-    let cells = rows.selectAll('td')
-        .data(row => d3.range(desireColumns.length)
-            .map((column, i) => row[desireColumns[i]]))
-        .enter()
-        .append('td')
-        .attr('class', 'customCell')
-        .text(d => d)
-        .on('mouseover', function (d, i) {            // remember this shit, dear reader...
-            d3.select(this.parentNode)                // in this place we couldn't use some lambdas. Why?
-                .style("background-color", "#F3ED86") // because keyword 'this' in lambdas determined by where lambda
-        })                                            // was defined. DEFINED - NOT USED, KARL!
-        .on('mouseout', function () {
-            tbody.selectAll('tr')
-                .style("background-color", null);
-            colorizeTableInZebraStyle(tbody);
-        });
-
-    colorizeTableInZebraStyle(tbody);
-}
-
-function onCbChanged(continent) {
-    console.log(continent)
-    console.log(d3.select('#cb' + continent).property('checked'))
-    cbContinentsState[continent] = d3.select('#cb' + continent).property('checked');
-
-    d3.select("table")
-        .select('tbody')
-        .selectAll('tr')
-        .remove();
-
-    drawTableRows(d3.select("table").select('tbody'),
-        desireColumns,
-        dataset_Countries2012.filter(e => Object.values(cbContinentsState).includes(true) ?
-            cbContinentsState[e['continent']] : true
-        ))
-}
-
-function onRbChanged(aggregation) {
-    var data = dataset_Countries2012
-    if (!d3.select('#rb' + aggregation).property('checked') &&
-        d3.select('#rb' + aggregation).attr('value') === 'None' ||
-        d3.select('#rb' + aggregation).property('checked') &&
-        d3.select('#rb' + aggregation).attr('value') === 'Continent' ){
-            data = aggregate();
-    }
-
-    d3.select("table")
-        .select('tbody')
-        .selectAll('tr')
-        .remove();
-
-    drawTableRows(d3.select('table').select('tbody'), desireColumns, data)
-}
-
-function aggregate() {
-    let nestedRows =  d3.nest()
-        .key(d => d['continent'])
-        .rollup(leaves => {
-            let initial = JSON.parse(JSON.stringify(leaves[0]));
-            d3.keys(initial).forEach(key => initial[key] = 0);
-            initial.name = initial.continent = leaves[0].continent;
-            initial.year = leaves[0].year;
-
-
-            return leaves.reduce( (prev, curr) => {
-                let ans = {};
-                d3.keys(prev).forEach(key => {
-                    switch (key) {
-                        case 'gdp':
-                            curr[key] = curr[key].toString();
-                            ans[key] = prev[key] += gdpParseFormatter(curr[key]);
-                            break;
-                        case 'population':
-                            // console.log(prev[key])
-                            ans[key] = prev[key] += populationParseFormatter(curr[key]);
-                            break;
-                        case 'life_expectancy':
-                            ans[key] = prev[key] += curr[key] / leaves.length;
-                            break;
-                        default:
-                            ans[key] = prev[key];
-                    }
-                });
-                return ans;
-            } , initial);
-        },)
-        .entries(dataset_Countries2012);
-
-    var ret = [];
-    nestedRows.map(e => e.value).forEach(x => ret.push(x));
-
-    ret.forEach(d => formatStringRepresent(d));
-    return ret;
 }
 
 function drawTableRows(body, columns, data){
@@ -186,9 +81,129 @@ function drawTableRows(body, columns, data){
             body.selectAll('tr')
                 .style("background-color", null);
             colorizeTableInZebraStyle(body);
-        });;
-
+        });
     colorizeTableInZebraStyle(body);
+}
+
+function rangeChangeYear(value){
+    dataset = changeData(value - d3.select('#rangeYear').property('min'));
+    dataset.forEach(o => formatStringRepresent(o));
+
+    let tableBody = d3.select('table').select('tbody');
+
+    tableCleaner(tableBody);
+    drawTableRows(tableBody, desireColumns, dataset)
+}
+
+function changeData(year) {
+    let data = [];
+
+    dataset_CountriesByYears.forEach(x => {
+        var a = {};
+        a['continent'] = x.continent;
+        a['name'] = x.name;
+        a['gdp'] = x.years[year].gdp !== null ? x.years[year].gdp : 0.0;
+        a['life_expectancy'] = x.years[year].life_expectancy;
+        a['population'] = x.years[year].population;
+        a['year'] = x.years[year].year;
+        data.push(a);
+    });
+
+    return data;
+}
+
+function drawTable() {
+    let table = tableDrawer(),
+        thead = tableHeadDrawer(table),
+        tbody = tableBodyDrawer(table);
+
+    tableCaptionDrawer(table);
+    addTableHeaders(thead, tbody);
+    drawTableRows(tbody, desireColumns, dataset);
+
+    colorizeTableInZebraStyle(tbody);
+}
+
+function drawTableByAllYears() {
+    dataset = changeData(0);
+    dataset.forEach(o => formatStringRepresent(o));
+
+    drawTable();
+}
+
+function drawTableByOneYear() {
+    dataset = dataset_Countries2012;
+    dataset.forEach(o => formatStringRepresent(o));
+
+    drawTable();
+}
+
+function onCbChanged(continent) {
+    cbContinentsState[continent] = d3.select('#cb' + continent).property('checked');
+
+    let tableBody = d3.select("table").select('tbody');
+
+    tableCleaner(tableBody);
+    drawTableRows(tableBody,
+        desireColumns,
+        dataset.filter(e => Object.values(cbContinentsState).includes(true) ?
+            cbContinentsState[e['continent']] : true
+        ))
+}
+
+function onRbChanged(aggregation) {
+    let data = dataset;
+    if (!d3.select('#rb' + aggregation).property('checked') &&
+        d3.select('#rb' + aggregation).attr('value') === 'None' ||
+        d3.select('#rb' + aggregation).property('checked') &&
+        d3.select('#rb' + aggregation).attr('value') === 'Continent' ){
+            data = aggregate();
+    }
+
+    let tableBody = d3.select("table").select('tbody');
+
+    tableCleaner(tableBody);
+    drawTableRows(d3.select('table').select('tbody'), desireColumns, data)
+}
+
+function aggregate() {
+    let nestedRows =  d3.nest()
+        .key(d => d['continent'])
+        .rollup(leaves => {
+            let initial = JSON.parse(JSON.stringify(leaves[0]));
+            d3.keys(initial).forEach(key => initial[key] = 0);
+            initial.name = initial.continent = leaves[0].continent;
+            initial.year = leaves[0].year;
+
+
+            return leaves.reduce( (prev, curr) => {
+                let ans = {};
+                d3.keys(prev).forEach(key => {
+                    switch (key) {
+                        case 'gdp':
+                            curr[key] = curr[key].toString();
+                            ans[key] = prev[key] += gdpParseFormatter(curr[key]);
+                            break;
+                        case 'population':
+                            ans[key] = prev[key] += populationParseFormatter(curr[key]);
+                            break;
+                        case 'life_expectancy':
+                            ans[key] = prev[key] += curr[key] / leaves.length;
+                            break;
+                        default:
+                            ans[key] = prev[key];
+                    }
+                });
+                return ans;
+            } , initial);
+        },)
+        .entries(dataset);
+
+    let ret = [];
+    nestedRows.map(e => e.value).forEach(x => ret.push(x));
+
+    ret.forEach(d => formatStringRepresent(d));
+    return ret;
 }
 
 function sorting(a, b, header) {
@@ -219,41 +234,12 @@ function formatStringRepresent(str) {
     str.population = d3.format(',')(str.population);
     str.life_expectancy = d3.format('.1f')(str.life_expectancy);
 
+    if(str.gdp < 1e9)
+        str.gdp = d3.formatPrefix(',.1', 1e6)(str.gdp);
     if(str.gdp >= 1e9 && str.gdp < 1e12)
         str.gdp = d3.formatPrefix(',.1', 1e9)(str.gdp);
     if(str.gdp >= 1e12 && str.gdp < 1e15)
         str.gdp = d3.formatPrefix(',.1', 1e12)(str.gdp);
     if(str.gdp >= 1e15)
         str.gdp = d3.formatPrefix(',.1', 1e15)(str.gdp);
-}
-
-function getNeededColumnKeys(columnsKeys) {
-    let neededColumnsKeys = [];
-
-    columnsKeys.forEach(x => {
-        switch (x) {
-            case 'continent' :
-                neededColumnsKeys.push(x);
-                break;
-            case 'name':
-                neededColumnsKeys.unshift(x);
-                break;
-            case 'gdp':
-                neededColumnsKeys.push(x);
-                break;
-            case 'life_expectancy':
-                neededColumnsKeys.push(x);
-                break;
-            case 'population':
-                neededColumnsKeys.push(x);
-                break;
-            case 'year':
-                neededColumnsKeys.push(x);
-                break;
-            default:
-                break;
-        }
-    });
-
-    return neededColumnsKeys;
 }
