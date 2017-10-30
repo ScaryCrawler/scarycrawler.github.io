6b3229751cb938f688a3a45f7f44cb2897145946
Ivan <sakhno.iv@gmail.com>
2017-10-30 17:17:42 +0300

    First working version of changeable bar chart

--- hw3/js/barChart.js
+++ hw3/js/barChart.js
@@ -17,27 +17,33 @@ class BarChart {
         this.allData = allData;
 
         this.svg = d3.select("#barChart");
+
+        this.margin = { top: 10, bottom: 30, right: 5, left: 50 };
+
+        this.timelineDictionary = {
+            attendance: 'AVERAGE_ATTENDANCE',
+            teams: 'TEAMS',
+            matches: 'MATCHES',
+            goals: 'GOALS'
+        };
+
+        this.animationSpeed = 800;
     }
 
     chartConfiguration(svgArea, xScaleParam, yScaleParam) {
-        let margin = {
-            top: 10,
-            bottom: 30,
-            right: 5,
-            left: 50
-        };
+        this.width = +svgArea.attr("width") -
+            this.margin.left - this.margin.right;
+        this.height = +svgArea.attr("height") -
+            this.margin.bottom - this.margin.top;
 
-        let width = +svgArea.attr("width") - margin.left - margin.right,
-            height = +svgArea.attr("height") - margin.bottom - margin.top;
-
-        this.x = d3.scaleBand().rangeRound([0, width]).padding(0.1);
-        this.y = d3.scaleLinear().rangeRound([height, 0]);
+        this.x = d3.scaleBand().rangeRound([0, this.width]).padding(0.1);
+        this.y = d3.scaleLinear().rangeRound([this.height, 0]);
 
         this.x.domain(fifaDataset.championshipDataset.map(d => d[xScaleParam]));
         this.y.domain([0, d3.max(fifaDataset.championshipDataset, d => d[yScaleParam])]);
 
         this.svgGroup = this.svg.append("g")
-            .attr("transform", "translate(" + margin.left + "," +  margin.top +  ")");
+            .attr("transform", "translate(" + this.margin.left + "," +  this.margin.top +  ")");
     }
 
     drawBarChart(svgGroup, xScale, yScale) {
@@ -46,29 +52,36 @@ class BarChart {
             .data(fifaDataset.championshipDataset)
             .enter()
             .append("rect")
+                .transition()
+                .duration(this.animationSpeed)
                 .attr("class", "bar")
                 .attr("x", d => this.x(d[xScale]))
                 .attr("y", d => this.y(d[yScale]))
                 .attr("width", this.x.bandwidth())
-                .attr("height", d => (this.svg.attr('height') - 30) - this.y(d[yScale])); // TODO: need to replace this hardcode
+                .attr("height", d => this.height - this.y(d[yScale])); // TODO: need to replace this hardcode
     }
 
     drawAxes(svgGroup) {
         svgGroup.append("g")
             .attr("class", "axisX")
-            .attr("transform", "translate(0," + (this.svg.attr('height') - 30) + ")")
+            .attr("transform", "translate(0," + this.height + ")")
             .call(d3.axisBottom(this.x));
 
         svgGroup.append("g")
             .attr("class", "axisY")
-            .attr("transform", "translate(0," + 10 + ")")
             .call(d3.axisLeft(this.y))
                 .append("text")
                 .attr("transform", "rotate(-90)")
                 // .attr("y", 0)
                 // .attr("dy", "0.71em")
                 .attr("text-anchor", "end")
-                .text("Frequency");
+    }
+
+    clearBarChart() {
+        this.svg
+            .selectAll("g")
+            .remove();
+            // .delay(this.animationSpeed);
     }
 
     /**
@@ -77,9 +90,11 @@ class BarChart {
     updateBarChart(selectedDimension) {
         // console.log(fifaDataset.championshipDataset);
 
-        this.chartConfiguration(this.svg, 'YEAR', 'AVERAGE_ATTENDANCE');
+        // this.clearBarChart();
+
+        this.chartConfiguration(this.svg, 'YEAR', selectedDimension);
         this.drawAxes(this.svgGroup);
-        this.drawBarChart(this.svgGroup, 'YEAR', 'AVERAGE_ATTENDANCE');
+        this.drawBarChart(this.svgGroup, 'YEAR', selectedDimension);
 
 
         // ******* TODO: PART I *******
@@ -92,9 +107,6 @@ class BarChart {
         // Create the axes (hint: use #xAxis and #yAxis)
 
         // Create the bars (hint: use #bars)
-
-
-
 
         // ******* TODO: PART II *******
 
@@ -117,6 +129,8 @@ class BarChart {
         // ******* TODO: PART I *******
         //Changed the selected data when a user selects a different
         // menu item from the drop down.
-        this.updateBarChart();
+        console.log(d3.select("#dataset")
+            .property('value'));
+        this.updateBarChart(this.timelineDictionary[d3.select("#dataset").property('value')]);
     }
 }
